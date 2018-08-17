278a938ecd466ed384e79c010bad2ee0de1e7ed1
chuang] <33791604+chuang861012@users.noreply.github.com>
2018-08-17 22:55:26 +0800

    add contextmenu

--- events.js
+++ events.js
@@ -1,5 +1,39 @@
 //安裝與更新的初始化
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        "title": "新增到屏蔽清單",
+        "id": "item1",
+        "type": "normal",
+        "contexts": ['link'],
+        "documentUrlPatterns": ["https://exhentai.org/g/*","https://e-hentai.org/g/*"]
+    });
+    chrome.contextMenus.onClicked.addListener(function (info) {
+        if (info.linkUrl.match("/uploader/")) {
+            const uploader = info.linkUrl.split("/").slice(-1)[0];
+            console.log(`Add ${uploader} to list.`);
+            chrome.storage.sync.get("Uploaders",
+                (list) => {
+                    let temp = list.Uploaders;
+                    temp[uploader] = "clear";
+                    chrome.storage.sync.set({
+                        Uploaders: temp
+                    })
+                });
+        } else if (info.linkUrl.match("/tag/")) {
+            const tag = info.linkUrl.split("/").slice(-1)[0].split("%3A").slice(-1)[0].replace("+", " ");
+            console.log(`Add ${tag} to list.`);
+            chrome.storage.sync.get("tags",
+                (list) => {
+                    let temp = list.tags;
+                    temp[tag] = "clear";
+                    chrome.storage.sync.set({
+                        tags: temp
+                    })
+                });
+        } else {
+            console.log("not tag or uploader.")
+        }
+    });
     chrome.storage.sync.get(null, (list) => {
         const tags = list.tags || {};
         const Uploaders = list.Uploaders || {};
